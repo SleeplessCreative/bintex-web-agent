130083c61aac70078cf3d83aae5b056100666a19
arridhow <arridhow@gmail.com>
2020-12-06 11:37:31 +0700

    :rocket: add tab, users data, and stuff

--- src/views/packages/packagesout/PackagesOut.js
+++ src/views/packages/packagesout/PackagesOut.js
@@ -1,7 +1,129 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CCard,
+  CCardBody,
+  CBadge,
+  CButton,
+  CCollapse,
+  CDataTable,
+} from "@coreui/react";
 
-const PackagesOut = () => {
-  return <>out</>;
+import usersDataOut from "../../../data/UsersDataOut";
+
+const PackagesIn = () => {
+  const [details, setDetails] = useState([]);
+  // const [items, setItems] = useState(usersData)
+
+  const toggleDetails = (index) => {
+    const position = details.indexOf(index);
+    let newDetails = details.slice();
+    if (position !== -1) {
+      newDetails.splice(position, 1);
+    } else {
+      newDetails = [...details, index];
+    }
+    setDetails(newDetails);
+  };
+
+  const fields = [
+    "resi",
+    "sender",
+    "receiver",
+    { key: "phone", label: "Phone Number" },
+    "from",
+    "to",
+    { key: "status", _style: { width: "10%" } },
+    {
+      key: "show_details",
+      label: "",
+      _style: { width: "2%" },
+      sorter: false,
+      filter: false,
+    },
+  ];
+
+  const getBadge = (status) => {
+    switch (status) {
+      case "Active":
+        return "success";
+      case "Inactive":
+        return "secondary";
+      case "Pending":
+        return "warning";
+      case "Banned":
+        return "danger";
+      default:
+        return "primary";
+    }
+  };
+
+  return (
+    <CCard className="overflow-hidden">
+      <CCardBody>
+        <CDataTable
+          items={usersDataOut}
+          fields={fields}
+          columnFilter
+          tableFilter
+          footer
+          itemsPerPageSelect
+          itemsPerPage={5}
+          hover
+          sorter
+          pagination
+          // loading
+          // onRowClick={(item,index,col,e) => console.log(item,index,col,e)}
+          // onPageChange={(val) => console.log('new page:', val)}
+          // onPagesChange={(val) => console.log('new pages:', val)}
+          // onPaginationChange={(val) => console.log('new pagination:', val)}
+          // onFilteredItemsChange={(val) => console.log('new filtered items:', val)}
+          // onSorterValueChange={(val) => console.log('new sorter value:', val)}
+          // onTableFilterChange={(val) => console.log('new table filter:', val)}
+          // onColumnFilterChange={(val) => console.log('new column filter:', val)}
+          scopedSlots={{
+            status: (item) => (
+              <td>
+                <CBadge color={getBadge(item.status)}>{item.status}</CBadge>
+              </td>
+            ),
+            show_details: (item) => {
+              return (
+                <td className="py-2">
+                  <CButton
+                    color="primary"
+                    variant="outline"
+                    shape="square"
+                    size="sm"
+                    onClick={() => {
+                      toggleDetails(item.id);
+                    }}
+                  >
+                    {details.includes(item.id) ? "Hide" : "Show"}
+                  </CButton>
+                </td>
+              );
+            },
+            details: (item) => {
+              return (
+                <CCollapse show={details.includes(item.id)}>
+                  <CCardBody>
+                    <h4>{item.username}</h4>
+                    <p className="text-muted">User since: {item.registered}</p>
+                    <CButton size="sm" color="info">
+                      User Settings
+                    </CButton>
+                    <CButton size="sm" color="danger" className="ml-1">
+                      Delete
+                    </CButton>
+                  </CCardBody>
+                </CCollapse>
+              );
+            },
+          }}
+        />
+      </CCardBody>
+    </CCard>
+  );
 };
 
-export default PackagesOut;
+export default PackagesIn;
