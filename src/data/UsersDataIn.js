63f82931226782a43c4411dbe3ce927dbec6f3ab
arridhow <arridhow@gmail.com>
2020-12-11 11:04:27 +0700

    :pencil: rename dir: userdata to data

---
+++ src/data/UsersDataIn.js
@@ -0,0 +1,13 @@
+const usersDataIn = [
+  {
+    resi: Math.floor(Math.random() * 1000000000),
+    sender: "Erald",
+    receiver: "Algi",
+    phone: "0822",
+    from: "medan",
+    to: "perawang",
+    status: "Pending",
+  },
+];
+
+export default usersDataIn;
---
+++ src/data/UsersDataOut.js
@@ -0,0 +1,22 @@
+const usersDataOut = [
+  {
+    resi: Math.floor(Math.random() * 1000000000),
+    sender: "Erald",
+    receiver: "Algi",
+    phone: "0822",
+    from: "medan",
+    to: "perawang",
+    status: "Pending",
+  },
+  {
+    resi: Math.floor(Math.random() * 1000000000),
+    sender: "Eraldsss",
+    receiver: "Algi",
+    phone: "0822",
+    from: "medan",
+    to: "perawang",
+    status: "Pending",
+  },
+];
+
+export default usersDataOut;
