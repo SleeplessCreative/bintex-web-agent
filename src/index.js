db40f88f3c128da9fa4ca3885e946d5dc98a26ee
arridhow <arridhow@gmail.com>
2021-01-02 14:28:25 +0700

    :rocket: add get cookie and redirect

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
-import 'react-app-polyfill/ie11'; // For IE 11 support
-import 'react-app-polyfill/stable';
-import './polyfill'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { icons } from './assets/icons'
+import "react-app-polyfill/ie11"; // For IE 11 support
+import "react-app-polyfill/stable";
+import "./polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import Cookies from "js-cookie";
 
-import { Provider } from 'react-redux'
-import store from './store'
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { icons } from "./assets/icons";
 
-React.icons = icons
+import { Provider } from "react-redux";
+import store from "./store";
 
+let token = Cookies.getJSON("acc");
+
+const url = "https://staging.bintex.id/api/is-auth";
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json;charset=UTF-8",
+    Authorization: token.token,
+  },
+};
+
+async function loggedIn() {
+  let res = await fetch(url, options);
+  let data = await res.status;
+  return data;
+}
+
+React.icons = icons;
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    {loggedIn()
+      .then(<App />)
+      .catch((window.location.href = "https://bintex.id/login"))}
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
