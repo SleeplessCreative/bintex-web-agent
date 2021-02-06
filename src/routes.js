89d3890e23a534fc7ae00f80bda2ff98e6dd09ca
arridhow <arridhow@gmail.com>
2021-02-06 10:57:21 +0700

    :rocket: add ongkir route

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ const Dashboard = React.lazy(() => import("./views/dashboard/Dashboard"));
 const PackagesIn = React.lazy(() => import("./views/packagesin/PackagesIn"));
 const PackagesOut = React.lazy(() => import("./views/packagesout/PackagesOut"));
 const Payment = React.lazy(() => import("./views/payment/Payment"));
-const Receipt = React.lazy(() => import("./views/receipt/Receipt"));
+const Invoice = React.lazy(() => import("./views/invoice/Invoice"));
+const Ongkir = React.lazy(() => import("./views/ongkir/Ongkir"));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
@@ -16,7 +17,8 @@ const routes = [
     component: PackagesOut,
   },
   { path: "/payment", name: "PAYMENT", component: Payment },
-  { path: "/receipt", name: "RECEIPT", component: Receipt },
+  { path: "/invoice", name: "INVOICE", component: Invoice },
+  { path: "/ongkir", name: "ONGKIR", component: Ongkir },
 ];
 
 export default routes;
